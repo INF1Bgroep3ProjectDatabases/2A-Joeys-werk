779c445a17ebf919ed425888ffd349004196a427
Itzjoey2003 <joeyberglund2003@gmail.com>
2022-03-10 11:00:30 +0100

    Opdracht 2A-Joey
    Joey's opdracht van week2

---
+++ Assignment 2-A, Joey's deel/Someren-master/SomerenModel/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Student
+    { 
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Number { get; set; } // StudentNumber, e.g. 474791
+        public DateTime BirthDate { get; set; }
+
+    
+    }
+}
